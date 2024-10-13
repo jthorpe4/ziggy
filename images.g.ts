1864744d282b737b9f363ac13df93065fa23d904
Jason Thorpe <jthorpe4.droid@gmail.com>
2024-10-13 22:35:58 +0100

    Added direction and hopefully wont get a merge conflict

--- images.g.ts
+++ images.g.ts
@@ -22,25 +22,6 @@ namespace myImages {
 . . f 4 f f 4 1 1 f 1 f . . . . 
 . . . f . f f f f f f f . . . . 
 `;
-            case "image3":
-            case "ziggyRight":return img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . f f . . . f f . . 
-. . . . . . . f 1 f f f 1 f . . 
-. . . . . . . f 1 4 4 4 1 f . . 
-. . . . . . . f 1 f 1 1 f f . . 
-. . . . . . . f 1 1 1 3 1 f . . 
-. . . . . . . f 4 1 1 1 1 f . . 
-. . . . . . f 4 4 1 1 1 f . . . 
-. . . . . . f 4 1 1 1 1 f . . . 
-. . . . f . f 4 1 1 1 1 f . . . 
-. . . f 4 f 4 1 1 f 1 f 4 f . . 
-. . f 4 f f 4 1 1 f 1 f 1 f . . 
-. . . f . f f f f f f f f f . . 
-`;
             case "image1":
             case "ziggyLeft":return img`
 . . . . . . . . . . . . . . . . 
@@ -59,6 +40,25 @@ namespace myImages {
 . . f 4 f 1 f 1 1 4 f 4 f . . . 
 . . f 1 f 1 f 1 1 4 f f 4 f . . 
 . . f f f f f f f f f . f . . . 
+`;
+            case "image3":
+            case "ziggyRight":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . f f . . 
+. . . . . . . f 1 f f f 1 f . . 
+. . . . . . . f 1 4 4 4 1 f . . 
+. . . . . . . f 1 f 1 1 f f . . 
+. . . . . . . f 1 1 1 3 1 f . . 
+. . . . . . . f 4 1 1 1 1 f . . 
+. . . . . . f 4 4 1 1 1 f . . . 
+. . . . . . f 4 1 1 1 1 f . . . 
+. . . . f . f 4 1 1 1 1 f . . . 
+. . . f 4 f 4 1 1 f 1 f 4 f . . 
+. . f 4 f f 4 1 1 f 1 f 1 f . . 
+. . . f . f f f f f f f f f . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    ziggySprite.setImage(assets.image`ziggyLeft`)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    ziggySprite.setImage(assets.image`ziggyRight`)
+})
+let ziggySprite: Sprite = null
 scene.setBackgroundColor(7)
 music.play(music.stringPlayable("E D G F B A C5 B ", 240), music.PlaybackMode.UntilDone)
 game.splash("Hello world")
-let ziggySprite = sprites.create(assets.image`ziggyLeft`, SpriteKind.Player)
+ziggySprite = sprites.create(assets.image`ziggyLeft`, SpriteKind.Player)
 controller.moveSprite(ziggySprite)
