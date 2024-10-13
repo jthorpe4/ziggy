0c1e8570962ce830f231005a284113904af26bc3
Jason Thorpe <jthorpe4.droid@gmail.com>
2024-10-13 22:19:25 +0100

    My Changes

--- main.ts
+++ main.ts
@@ -1,5 +1,22 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    Direction = "U"
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    ziggySprite.setImage(assets.image`myImage1`)
+    Direction = "L"
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    ziggySprite.setImage(assets.image`myImage`)
+    Direction = "R"
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    Direction = "D"
+})
+let Direction = ""
+let ziggySprite: Sprite = null
 scene.setBackgroundColor(7)
 music.play(music.stringPlayable("E D G F B A C5 B ", 240), music.PlaybackMode.UntilDone)
 game.splash("Hello world")
-let ziggySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+ziggySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
 controller.moveSprite(ziggySprite)
+Direction = "R"
