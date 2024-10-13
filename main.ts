f2d4f16164871d43deeef54788465a0e9a338761
Jason Thorpe <jthorpe4.droid@gmail.com>
2024-10-13 22:39:10 +0100

    Hopefully works this time

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    ziggySprite.sayText(":)", 500, false)
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     ziggySprite.setImage(assets.image`ziggyLeft`)
 })
@@ -6,7 +9,6 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 let ziggySprite: Sprite = null
 scene.setBackgroundColor(7)
-music.play(music.stringPlayable("E D G F B A C5 B ", 240), music.PlaybackMode.UntilDone)
 game.splash("Hello world")
 ziggySprite = sprites.create(assets.image`ziggyLeft`, SpriteKind.Player)
 controller.moveSprite(ziggySprite)
